02ff3ce231325ae2124877141777825347381034
Vi-Linh Nguyen <58266005+ViLinh27@users.noreply.github.com>
2020-01-22 16:03:44 -0600

    I changed some of the methods
    The function for calculating certain deviations away from the mean were separated into three different functions to isolate the desired value more easily.

--- Distribution.java
+++ Distribution.java
@@ -14,6 +14,9 @@ public class Distribution {
 		//print out everything:
 		double mean = calcMean(num);
 		double sd=calcDeviation(num);
+		double sd1 = sd1Away(num);
+		double sd2 = sd2Away(num);
+		double sd3 = sd3Away(num);
 		/*
 		 Need to find condition for 1,2,3 SD away from mean
 		 */
@@ -23,15 +26,15 @@ public class Distribution {
 		//what I should get as range:
 		/* Approximately 68% of the data values will fall within 1 standard deviation of the mean, from 
 		 -1.0160800091362 to 1.0071442759868.*/
-		StdOut.println("\nPercentage of values 1 SD away from mean: ");
+		StdOut.println("\nPercentage of values 1 SD away from mean: "+sd1);
 		//what I should get as range:
 		/*Approximately 95% of the data values will fall within 2 standard deviations of the mean, from 
 		 -2.0276921516977 to 2.0187564185484.*/
-		StdOut.println("\nPercentage of values 2 SD away from mean: ");
+		StdOut.println("\nPercentage of values 2 SD away from mean: "+sd2);
 		//what I should get as range:
 		/*Approximately 99.7% of the data values will fall within 3 standard deviations of the mean, from 
 		-3.0393042942593 to 3.0303685611099.*/
-		StdOut.println("\nPercentage of values 3 SD away from mean: " );
+		StdOut.println("\nPercentage of values 3 SD away from mean: "+sd3);
 		StdOut.println("\n");
 	}
 	
@@ -63,16 +66,43 @@ public class Distribution {
 		
 		return sdev;
 	}
-	public static void sdAway(double[]num) {//make sure to change return type to double and add return statement eventually
+	public static double sd1Away(double[]num) {
 		double cMean = calcMean(num);
-		double sd1 = calcDeviation(num)-cMean;
-		double sd1Top = calcDeviation(num)+cMean;
-		double sd2 = calcDeviation(num)-(2*cMean);
-		double sd2Top= calcDeviation(num)+(2*cMean);
-		double sd3;
-		double sd3Top;
-		//percent 1 SD
-		//percent 2 SD
-		//percent 3 SD
+		double sd1 = calcMean(num)-calcDeviation(num);
+		double sd1Top =calcMean(num)+calcDeviation(num);
+		double perc1Count = 0;
+		for (double val: num) {
+			if(val<=sd1Top && val>=sd1) {
+				perc1Count++;
+			}
+		}
+		double perc1 = (perc1Count/num.length)*100;
+		return perc1;
+	}
+	public static double sd2Away(double[]num) {
+		double cMean = calcMean(num);
+		double sd2 =calcMean(num)-(2*calcDeviation(num));
+		double sd2Top=calcMean(num)+(2*calcDeviation(num));
+		double  perc2Count = 0;
+		for (double val: num) {
+			if(val<=sd2Top && val>=sd2) {
+				perc2Count++;
+			}
+		}
+		double perc2 = (perc2Count/num.length)*100;
+		return perc2;
+	}
+	public static double sd3Away(double[] num) {
+		double cMean = calcMean(num);
+		double sd3 =calcMean(num)-(3*calcDeviation(num));
+		double sd3Top=calcMean(num)+(3*calcDeviation(num));
+		double perc3Count = 0;
+		for(double val: num) {
+			if(val<=sd3Top && val>=sd3) {
+				perc3Count++;
+			}
+		}
+		double perc3 = (perc3Count/num.length)*100;
+		return perc3;
 	}
 }
